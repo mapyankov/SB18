2d84d9c6002f3dc0b787a605239efd70255275f8
mapyankov <126340359+mapyankov@users.noreply.github.com>
2023-10-21 15:15:03 +0300

    Создал своего бота

---
+++ print('hi').py
@@ -0,0 +1,28 @@
+import discord
+from discord.ext import commands
+import random
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Привет! Я бот {bot.user}!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+@bot.command()
+async def joined(ctx, member: discord.Member):
+    """Says when a member joined."""
+    await ctx.send(f'{member.name} joined {discord.utils.format_dt(member.joined_at)}')
+
+bot.run("MTE2MjcyNTEwNDk0MjMyNTc4MA.GT7sQN.4HPoXjW6ACO4t86tdd6BDDF57SK1-FFLuLoTYU")
+
